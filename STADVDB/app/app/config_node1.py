b8ceafa4601d0391d9ccc473a86d1c77c40e5ad8
Andrei Tamse <113184924+Brasprin@users.noreply.github.com>
2024-12-03 19:53:23 +0800

    Update config_node1.py

--- STADVDB/app/app/config_node1.py
+++ STADVDB/app/app/config_node1.py
@@ -2,5 +2,5 @@ class Config:
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:12345@127.0.0.1:3308/steamGames"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_BINDS = {
-    'master': 'mysql+pymysql://root:12345@127.0.0.1:3308/steamGames'  
+    'node1': 'mysql+pymysql://root:12345@127.0.0.1:3308/steamGames'  
     }
