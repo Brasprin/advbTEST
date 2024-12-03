55926a57f29934ec4bcd6f6e9c24e878151e3475
Andrei Tamse <113184924+Brasprin@users.noreply.github.com>
2024-12-03 18:52:35 +0800

    Add files via upload

---
+++ STADVDB/app/app/config_node1.py
@@ -0,0 +1,3 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:12345@127.0.0.1:3308/steamGames"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  
---
+++ STADVDB/app/app/config_node2.py
@@ -0,0 +1,3 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:12345@127.0.0.1:3309/steamGames"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
